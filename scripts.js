8679bcb38d20caf7407b807cc935561d09bbb436
Rafael Perger <rafael.perger@gmail.com>
2022-01-30 15:37:42 +0100

    Implement game function

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ const options = [{name: 'Rock', nemesis: 'Paper'},
     {name: 'Scissors', nemesis: 'Rock'}
 ];
 
- console.log(playRound('SciSsORs', computerPlay(options)));
- 
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0; 
+
+game();
+
 function computerPlay(options) {
     return options[Math.floor(Math.random()*options.length)];
 }
@@ -14,12 +18,37 @@ function playRound (playerSelection, computerSelection) {
     const playerSelectionObj = options.filter(obj => arrangedPlayerSelection === obj.name)[0];
     
     if (computerSelection.name === playerSelectionObj.nemesis) {
+        computerScore++;
         return (`You lose! ${computerSelection.name} beats ${playerSelectionObj.name}`)
     } else if (computerSelection.name === playerSelectionObj.name) {        
+        draw++;
         return (`It's a draw!${computerSelection.name} and ${playerSelectionObj.name} are equal`)
-    } else {        
+    } else { 
+        playerScore++;       
         return (`You win! ${playerSelectionObj.name} beats ${computerSelection.name}`)
         }
+}
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    draw = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        const playerInput = window.prompt('Choose Rock, Paper or Scissor and get ready for the battle');
+        const computerChoice = computerPlay(options);
+
+        console.log(playRound(playerInput, computerChoice))
+    }
+
+    if (playerScore < computerScore) {
+        console.log(`You've lost the game, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
+    }
+    else if (playerScore > computerScore) {
+        console.log(`You've won the game, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
+    } else {
+        console.log(`It's a draw, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
+    }
 }
 
 function capitalize(strng) {
