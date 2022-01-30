ada4670ed9fac6ce9cc860ef0d3ee5e161d21891
Rafael Perger <rafael.perger@gmail.com>
2022-01-30 15:04:09 +0100

    Make playRound function.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
-const options = ['Rock', 'Paper', 'Scissors'];
+const options = [{name: 'Rock', nemesis: 'Paper'},
+    {name: 'Paper', nemesis: 'Scissors'},
+    {name: 'Scissors', nemesis: 'Rock'}
+];
 
-console.log(computerPlay(options));
+playRound('Scissors', computerPlay(options));
 
 function computerPlay(options) {
     return options[Math.floor(Math.random()*options.length)];
+}
+
+function playRound (playerSelection, computerSelection) {
+    const playerSelectionObj = options.filter(obj => playerSelection === obj.name)[0];
+    
+    if (computerSelection.name === playerSelectionObj.nemesis) {
+        console.log(`You lose! ${computerSelection.name} beats ${playerSelectionObj.name}`)
+    } else if (computerSelection.name === playerSelectionObj.name) {        
+        console.log(`It's a draw!${computerSelection.name} and ${playerSelectionObj.name} are equal`)
+    } else {        
+        console.log(`You win! ${playerSelectionObj.name} beats ${computerSelection.name}`)
+        }
 }
