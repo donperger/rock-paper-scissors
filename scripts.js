c46c650f71d6bd6def510576b47c42abdb9c6ee8
Rafael Perger <rafael.perger@gmail.com>
2022-01-30 14:18:41 +0100

    Write computerPlay function.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log('scripts.js works fine')
+const options = ['Rock', 'Paper', 'Scissors'];
+
+console.log(computerPlay(options));
+
+function computerPlay(options) {
+    return options[Math.floor(Math.random()*options.length)];
+}
