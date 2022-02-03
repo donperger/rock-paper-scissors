b2e58ee323c7b0d9a3193e0645bb8032b0e505e2
Rafael Perger <rafael.perger@gmail.com>
2022-02-03 16:45:52 +0100

    Delete game function from scripts.js.

--- scripts.js
+++ scripts.js
@@ -54,26 +54,6 @@ function playRound (playerSelection, computerSelection) {
         }
 }
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    draw = 0;
-
-    // const playerInput = window.prompt('Choose Rock, Paper or Scissor and get ready for the battle');
-    const computerChoice = computerPlay(options);
-
-    console.log(playRound(playerInput, computerChoice))
-
-    if (playerScore < computerScore) {
-        console.log(`You've lost the game, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
-    }
-    else if (playerScore > computerScore) {
-        console.log(`You've won the game, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
-    } else {
-        console.log(`It's a draw, You scored ${playerScore}, the computer scored ${computerScore} (draw: ${draw}), see you next time!`);
-    }
-}
-
 function capitalize(strng) {
     return strng[0].toUpperCase() + strng.slice(1).toLowerCase()
   }
